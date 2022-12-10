b737ab74aa604428a6bed76b59a7a43875f7b262
Angelelz <angellopez20@gmail.com>
2022-12-09 20:11:12 -0500

    details

--- 10/index.js
+++ 10/index.js
@@ -2,8 +2,8 @@ const input = require("./input").split("\n");
 
 const answer1 = 1;
 
-module.exports.one = `Answer 9.1 is ${answer1}`;
+module.exports.one = `Answer 10.1 is ${answer1}`;
 
 const answer2 = 2;
 
-module.exports.two = `Answer 9.2 is ${answer2}`;
+module.exports.two = `Answer 10.2 is ${answer2}`;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const answers = []
 const dirs = fs.readdirSync("./", {withFileTypes: true})
   .filter(dir => dir.isDirectory() && !dir.name.startsWith("."))
   .map(dir => dir.name)
+  .sort((a, b) => +a - +b)
 
 dirs.forEach(dir => {
   answers.push(require(`./${dir}`).one)
